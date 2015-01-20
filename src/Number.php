d636fbfc575b68457bc9927376ca33ca320e800e
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-20 21:20:24 +0100

    cleanup

--- src/Number.php
+++ src/Number.php
@@ -2,26 +2,8 @@
 
 namespace Formulaic;
 
-class Numeric extends Text
+class Number extends Text
 {
-    public function __set($name, $value)
-    {
-        if ($name != 'value') {
-            return;
-        }
-        $value = preg_replace('/[^0-9.,]/', '', $value);
-        // Guesstimate if this uses a . or , as decimal separator:
-        if (strrpos($value, '.') || strrpos($value, ',')) {
-            if (strrpos($value, '.') > strrpos($value, ',')) {
-                $value = str_replace(',', '', $value);
-            } else {
-                $value = str_replace('.', '', $value);
-                $value = str_replace(',', '.', $value);
-            }
-        } else {
-            $value = str_replace(['.', ','], '', $value);
-        }
-        return parent::__set($name, $value);
-    }
+    protected $attributes = ['type' => 'number'];
 }
 
