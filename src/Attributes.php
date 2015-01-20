cd721372b3a49f8f9d59136a0a75a1095c29aa2b
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-20 20:08:00 +0100

    add method to set single attribute, and use non-expanded syntax for empty attributes

--- src/Attributes.php
+++ src/Attributes.php
@@ -8,9 +8,23 @@ trait Attributes
     {
         $return = [];
         foreach ($this->attributes as $name => $value) {
-            $return[] = sprintf('%s="%s"', $name, htmlentities($value));
+            if (is_null($value)) {
+                $return[] = $name;
+            } else {
+                $return[] = sprintf('%s="%s"', $name, htmlentities($value));
+            }
         }
         return $return ? ' '.implode(' ', $return) : '';
+    }
+
+    public function attribute($name, $value = null)
+    {
+        if ($value === false) {
+            unset($this->attributes[$name]);
+        } else {
+            $this->attributes[$name] = $value;
+        }
+        return $this;
     }
 }
 
