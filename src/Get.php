293dc3177b0ff333e00829f8323cecdd430c4376
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-20 18:45:23 +0100

    use simple array for this

---
+++ src/Get.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Formulaic;
+
+class Get extends Form
+{
+    protected $attributes = ['method' => 'get'];
+
+    public function __construct($id = null)
+    {
+        parent::__construct($id);
+        $this->addSource($_GET);
+    }
+
+    public function cancelled()
+    {
+        return isset($_GET['act_cancel']);
+    }
+
+    public function getMethod()
+    {
+        return 'get';
+    }
+}
+
