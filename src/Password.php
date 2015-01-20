9dd7e128385a8c237ed252c940e2a31b12a381c4
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-20 21:39:44 +0100

    modernize: how the client does password validation is not our concern

--- src/Password.php
+++ src/Password.php
@@ -6,29 +6,7 @@ use ErrorException;
 
 class Password extends Text
 {
-    protected $type = 'password';
-
-    /** The field must equal the value supplied. */
-    public function isEqualTo($hashed)
-    {
-        try {
-            list($hashed, $salt) = func_get_args();
-        } catch (ErrorException $e) {
-            $salt = null;
-        }
-        return $this->addTest(function($value) use($hashed, $salt) {
-            if (!strlen(trim($value))) {
-                return null;
-            }
-            try {
-                list($hash, $pw) = explode(':', $hashed, 2);
-            } catch (ErrorException $e) {
-                return $value == $hashed ? null : self::ERROR_NOMATCH;
-            }
-            $value = "$hash:".hash($hash, $value.$salt);
-            return $value == $hashed ? null : self::ERROR_NOMATCH;
-        });
-    }
+    protected $attributes = ['type' => 'password'];
 
     public function __toString()
     {
