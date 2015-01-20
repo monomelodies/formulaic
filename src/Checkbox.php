7ed0cc43aaf0530a2df2c8ed7315ac8fc0f91689
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-20 21:20:51 +0100

    modernize

--- src/Checkbox.php
+++ src/Checkbox.php
@@ -4,11 +4,6 @@ namespace Formulaic;
 
 class Checkbox extends Radio
 {
-    protected $type = 'checkbox';
-
-    public function getValue()
-    {
-        return $this->options['value'];
-    }
+    protected $attributes = ['type' => 'checkbox'];
 }
 
