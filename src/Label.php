6e3fef5d09f955e449a38d46d88cd6e4895d6521
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-20 20:52:32 +0100

    prepare this for new style Formulaic

---
+++ src/Label.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Formulaic;
+
+class Label extends Element
+{
+    use Label\Tostring;
+
+    protected $label;
+    protected $element;
+
+    public function __construct($label, Element $element)
+    {
+        $this->label = $label;
+        $this->element = $element;
+    }
+
+    public function getElement()
+    {
+        return $this->element;
+    }
+
+    public function raw()
+    {
+        return $this->txt;
+    }
+}
+
