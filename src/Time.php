df6a5371cd2bbbd92919780083d043a114964e03
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-21 10:30:29 +0100

    fix constructor and tests

--- src/Time.php
+++ src/Time.php
@@ -6,9 +6,13 @@ class Time extends Element
 {
     protected $attributes = ['type' => 'time'];
 
-    public function isValidTime()
+    public function __construct($name = null)
     {
-        return $this->addTest(function ($value) {
+        parent::__construct($name);
+        $this->addTest(function ($value) {
+            if (is_null($value)) {
+                return null;
+            }
             $current = str_replace(':', '', $value);
             return $current > '000000' && $current < '235959' ?
                 null :
@@ -16,7 +20,16 @@ class Time extends Element
         });
     }
 
-    public function isTimeInPast()
+    public function setValue($value)
+    {
+        $totime = is_numeric($value) ? $value : strtotime($value);
+        if (!$totime) {
+            return parent::setValue($value);
+        }
+        return parent::setValue(date('H:i:s', $totime));
+    }
+
+    public function isInPast()
     {
         return $this->addTest(function ($value) { 
             return str_replace(':', '', $value) < date('His') ?
@@ -25,7 +38,7 @@ class Time extends Element
         });
     }
 
-    public function isTimeInFuture()
+    public function isInFuture()
     {
         return $this->addTest(function ($value) {
             return str_replace(':', '', $value) > date('His') ?
