63130ef690ea1787c1ea5da0809337aff6714251
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-21 10:36:02 +0100

    fix validity check and add constructor

--- src/Tel.php
+++ src/Tel.php
@@ -6,15 +6,28 @@ class Tel extends Text
 {
     protected $attributes = ['type' => 'tel'];
 
+    public function __construct($name = null)
+    {
+        parent::__construct($name);
+        $this->addTest(function ($value) {
+            return preg_replace('/[^\d]/', '', $value) == $value ?
+                null :
+                'error.nonnumeric';
+        });
+    }
+
     public function setValue($value)
     {
-        $value = preg_replace('/^\d/', '', $value);
-        if (strlen($value)) {
-            if ($value{0} != '0') {
-                $value = "0$value";
+        if (!is_null($value)) {
+            $tmp = preg_replace('/[^\d]/', '', $value);
+            if (strlen($tmp)) {
+                $value = $tmp;
+                if ($value{0} != '0') {
+                    $value = "0$value";
+                }
             }
         }
-        return parent::setValue($name, $value);
+        return parent::setValue($value);
     }
 }
 
