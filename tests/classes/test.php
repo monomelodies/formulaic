89a19ede00207c633bc7a6200b8c51901c3fd0c1
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-21 09:45:42 +0100

    let's just fill the forms in our test cases, perfectly valid

--- tests/classes/test.php
+++ tests/classes/test.php
@@ -6,34 +6,7 @@ use Formulaic\Text;
 use Formulaic\Button\Submit;
 use Formulaic\Fieldset;
 
-class EmptyForm extends Get
+class Form extends Get
 {
-}
-
-class SimpleForm extends Get
-{
-    public function __construct()
-    {
-        $this[] = new Text;
-        $this[] = new Submit;
-    }
-}
-
-class FieldsetForm extends Get
-{
-    public function __construct()
-    {
-        $this[] = new Fieldset('Hello world!', function($fieldset) {
-            $fieldset[] = new Text;
-        });
-    }
-}
-
-class SimpleFormWithName extends Get
-{
-    public function __construct()
-    {
-        $this[] = new Text('mytextfield');
-    }
 }
 
