4c25d19618967b55966293560dbb28e870320d48
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-21 10:08:07 +0100

    encode as UTF-8 by default

--- src/Attributes.php
+++ src/Attributes.php
@@ -14,7 +14,11 @@ trait Attributes
                 }
                 $return[] = $name;
             } else {
-                $return[] = sprintf('%s="%s"', $name, htmlentities($value));
+                $return[] = sprintf(
+                    '%s="%s"',
+                    $name,
+                    htmlentities($value, ENT_COMPAT, 'UTF-8')
+                );
             }
         }
         return $return ? ' '.implode(' ', $return) : '';
