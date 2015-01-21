96df4592fd2f5f5667c28dd3939fe78798446f3c
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-21 10:50:04 +0100

    v1 of implementing number inputs properly

--- src/Number.php
+++ src/Number.php
@@ -5,5 +5,28 @@ namespace Formulaic;
 class Number extends Text
 {
     protected $attributes = ['type' => 'number'];
+
+    public function __construct($name = null)
+    {
+        parent::__construct($name);
+        $this->addTest('numeric', 'is_numeric');
+    }
+
+    public function setMin($min)
+    {
+        $this->attributes['min'] = $min;
+        return $this;
+    }
+
+    public function setMax($max)
+    {
+        $this->attributes['max'] = $max;
+        return $this;
+    }
+
+    public function setStep($step)
+    {
+        $this->attributes['step'] = $step;
+    }
 }
 
