9bc7333cc2e207e03e8d049e96cc347f3a88b06a
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-21 18:02:40 +0100

    make abstract, and override populate to add $_GET

--- src/Get.php
+++ src/Get.php
@@ -2,12 +2,18 @@
 
 namespace Formulaic;
 
-class Get extends Form
+abstract class Get extends Form
 {
     public function __toString()
     {
         $this->attributes['method'] = 'get';
         return parent::__toString();
+    }
+
+    public function populate()
+    {
+        $this->source($_GET);
+        parent::populate();
     }
 }
 
