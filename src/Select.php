58697898833c7af9e2ffc6a87968546fdd3bb6d3
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-21 20:48:06 +0100

    this could use some cleanup

--- src/Select.php
+++ src/Select.php
@@ -34,40 +34,5 @@ class Select extends ArrayObject
             return false;
         });
     }
-
-    /*
-    public function getChoices()
-    {
-        return $this->choices;
-    }
-
-    public function __set($name, $value)
-    {
-        if ($name != 'value') {
-            return null;
-        }
-        if (isset($this->options['required']) && $this->options['required']) {
-            if (!strlen(trim($value))) {
-                $value = null;
-            }
-        }
-        $this->value = $value;
-        foreach ($this->choices as $choice) {
-            if ("{$choice->value}" == "$value") {
-                $choice->selected();
-            } else {
-                $choice->unselected();
-            }
-        }
-        return $value;
-    }
-
-    public function notNull()
-    {
-        if (isset($this->choices[0]) && $this->choices[0]->value === '') {
-            unset($this->choices[0]);
-        }
-    }
-    */
 }
 
