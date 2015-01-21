1dd91e7c55555ef94b96b545c91fe58825a10b52
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-21 16:40:16 +0100

    correct output and set for attribute

--- src/Label/Tostring.php
+++ src/Label/Tostring.php
@@ -8,18 +8,17 @@ trait Tostring
 {
     public function __toString()
     {
-        /*
-        if ($id = $this->element->getId()) {
+        if ($id = $this->element->id()) {
             $this->attributes['for'] = $id;
         }
-        */
         $out = '<label'.$this->attributes().'>';
         if ($this->element instanceof Radio) {
             $out .= "{$this->element} {$this->label}";
+            $out .= '</label>';
         } else {
-            $out .= "{$this->label} {$this->element}";
+            $out .= "{$this->label}</label>\n";
+            $out .= $this->element;
         }
-        $out .= '</label>';
         return $out;
     }
 }
