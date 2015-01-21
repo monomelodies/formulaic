546e728d54db38abc977e6623052cb5daa023dac
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-21 16:53:51 +0100

    handle method in toString

--- src/Get.php
+++ src/Get.php
@@ -4,24 +4,10 @@ namespace Formulaic;
 
 class Get extends Form
 {
-    protected $attributes = ['method' => 'get'];
-
-/*
-    public function __construct($id = null)
+    public function __toString()
     {
-        parent::__construct($id);
-        $this->addSource($_GET);
-    }
-*/
-
-    public function cancelled()
-    {
-        return isset($_GET['act_cancel']);
-    }
-
-    public function getMethod()
-    {
-        return 'get';
+        $this->attributes['method'] = 'get';
+        return parent::__toString();
     }
 }
 
