183d7768b7c9dc467c36c35ea616520d6eb7c642
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-21 20:12:50 +0100

    bunch of fixes here...

--- src/Checkbox/Group.php
+++ src/Checkbox/Group.php
@@ -3,27 +3,55 @@
 namespace Formulaic\Checkbox;
 
 use Formulaic;
+use Formulaic\Element;
+use Formulaic\Attributes;
+use Formulaic\Validate;
+use Formulaic\Checkbox;
+use Formulaic\Label;
 use ArrayObject;
 
-class Group extends ArrayObject
+class Group extends Element\Group
 {
-    use Formulaic\Attributes;
-    use Formulaic\Validate\Group;
+    use Attributes;
+    use Validate\Group;
     use Group\Tostring;
     
     protected $attributes = [];
+    private $prefix = [];
     
     public function __construct($name, $options)
     {
         if (is_callable($options)) {
             $options($this);
+            foreach ((array)$this as $option) {
+                foreach ($this->prefix as $prefix) {
+                    $option->prefix($prefix);
+                }
+                $option->prefix($name);
+            }
         } else {
             foreach ($options as $value => $txt) {
-                $option = new Formulaic\Checkbox;
+                $option = new Checkbox($value);
                 $option->setValue($value);
-                $this[] = new Formulaic\Label($txt, $option);
+                foreach ($this->prefix as $prefix) {
+                    $option->prefix($prefix);
+                }
+                $option->prefix($name);
+                $this[] = new Label($txt, $option);
             }
         }
+    }
+
+    public function isRequired()
+    {
+        return $this->addTest(function ($value) {
+            foreach ((array)$this as $option) {
+                if ($option->checked()) {
+                    return true;
+                }
+            }
+            return false;
+        });
     }
 }
 
