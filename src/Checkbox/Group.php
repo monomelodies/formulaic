9015b631d1aa6b92c63380859a5e96c441c0daf8
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-21 16:03:38 +0100

    sort-of make this work

---
+++ src/Checkbox/Group.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Formulaic\Checkbox;
+
+use Formulaic;
+use ArrayObject;
+
+class Group extends ArrayObject
+{
+    use Formulaic\Attributes;
+    use Formulaic\Validate\Group;
+    use Group\Tostring;
+    
+    protected $attributes = [];
+    
+    public function __construct($name, $options)
+    {
+        if (is_callable($options)) {
+            $options($this);
+        } else {
+            foreach ($options as $value => $txt) {
+                $option = new Formulaic\Checkbox;
+                $option->setValue($value);
+                $this[] = new Formulaic\Label($txt, $option);
+            }
+        }
+    }
+}
+
