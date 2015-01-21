895e59eb51b84d3f8d94e66accf6c19edbd0a2d8
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-21 18:02:23 +0100

    move more stuff into helper traits

--- src/Form.php
+++ src/Form.php
@@ -9,111 +9,17 @@ abstract class Form extends ArrayObject
     use Attributes;
     use Form\Tostring;
     use Validate\Group;
+    use InputHelper;
+    use QueryHelper;
 
     protected $attributes = [];
+    private $source = [];
 
     public function name()
     {
         return isset($this->attributes['name']) ?
             $this->attributes['name'] :
             null;
-    }
-
-    public function prepare()
-    {
-        if ($class = $this->classname()) {
-            $this->attributes += compact('class');
-        }
-        return $this->load();
-    }
-
-    public function load()
-    {
-        $data = [];
-        $r = function(&$source, $keys, $key, &$values) use(&$r) {
-            $keys[] = "[$key]";
-            $skey = implode('', $keys);
-            if (isset($this[$skey]) || !is_array($values)) {
-                $source[$skey] = $values;
-                return;
-            }
-            foreach ($values as $skey => $svalues) {
-                $r($source, $keys, $skey, $svalues);
-            }
-        };
-        foreach ($this->sources as $source) {
-            if ($source != $_FILES) {
-                foreach ($source as $name => $value) {
-                    if (is_array($value)
-                        && (!isset($this[$name])
-                            || $this[$name] instanceof Media
-                        )
-                    ) {
-                        $reduced = false;
-                        foreach ($value as $key => $v) {
-                            $reduced = true;
-                            $r($source, [$name], $key, $v);
-                        }
-                        if ($reduced) {
-                            unset($source[$name]);
-                        }
-                    }
-                }
-            }
-            $data += $source;
-        }
-        foreach ($data as $name => $value) {
-            if (!isset($this[$name])
-                || !($this[$name] instanceof Element)
-            ) {
-                continue;
-            }
-            if ($this[$name] instanceof Radio) {
-                if ($value == $this[$name]->__get('value')) {
-                    $this[$name]->checked();
-                } else {
-                    $this[$name]->unchecked();
-                }
-            } else {
-                $this[$name]->__set('value', $value);
-            }
-        }
-        return $this;
-    }
-
-    public function getAction()
-    {
-        return $this->action;
-    }
-
-    public function setAction($action)
-    {
-        $this->action = $action;
-    }
-
-    public function hasFiles()
-    {
-        foreach ($this as $key => $value) {
-            if ($value instanceof File) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public function addSource($source)
-    {
-        if (is_null($source)) {
-            return $this;
-        }
-        if (is_callable($source)) {
-            $source = $source();
-        }
-        if (!is_array($source) && is_object($source)) {
-            $source = $source->getArrayCopy();
-        }
-        $this->sources[] = $source;
-        return $this;
     }
 
     public function offsetGet($index)
