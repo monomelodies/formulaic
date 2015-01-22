74553b75dd1da085650fd078c90ae8eb3607f339
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-22 14:39:06 +0100

    custom populate method needed for checkbox groups

--- src/Checkbox/Group.php
+++ src/Checkbox/Group.php
@@ -7,5 +7,20 @@ use Formulaic\Radio;
 class Group extends Radio\Group
 {
     use Group\Tostring;
+
+    public function populate()
+    {
+        parent::populate();
+        if (!is_array($this->source)) {
+            return;
+        }
+        foreach ((array)$this as $element) {
+            if (in_array($element->getValue(), $this->source)) {
+                $element->check();
+            } else {
+                $element->check(false);
+            }
+        }
+    }
 }
 
