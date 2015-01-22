4694907800da60cd867f80a0d622d222dcea8944
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-22 14:48:11 +0100

    text tests in text

--- src/Text.php
+++ src/Text.php
@@ -11,5 +11,30 @@ class Text extends Element
         $this->attributes['size'] = $size;
         return $this;
     }
+
+    /** The field must match the pattern supplied. */
+    public function matchPattern($pattern)
+    {
+        $this->attributes['pattern'] = $pattern;
+        return $this->addTest('pattern', function ($value) use ($pattern) {
+            if (!isset($value)) {
+                return true;
+            }
+            return preg_match("@^$pattern$@", trim($value));
+        });
+    }
+    
+    /** The maximum length of the field. */
+    public function maxLength($length, $size = null)
+    {
+        if (!isset($size)) {
+            $size = min(32, $length);
+        }
+        $this->attributes['maxlength'] = (int)$length;
+        $this->attributes['size'] = (int)$size;
+        return $this->addTest('maxlength', function($value) use ($length) {
+            return mb_strlen(trim($value), 'UTF-8') <= (int)$length;
+        });
+    }
 }
 
