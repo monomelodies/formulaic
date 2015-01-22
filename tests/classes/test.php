e43050857696e7652100f6b378f9d4bbc9aecad3
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-22 16:24:58 +0100

    add complex testclass

--- tests/classes/test.php
+++ tests/classes/test.php
@@ -16,3 +16,25 @@ class NamedForm extends Get
     protected $attributes = ['name' => 'test'];
 }
 
+class ComplexForm extends Post
+{
+    public function __construct()
+    {
+        $this[] = new Formulaic\Label(
+            'Test',
+            (new Formulaic\Text('foo'))->isRequired()
+        );
+        $this[] = new Formulaic\Label(
+            'Group of radio buttons',
+            (new Formulaic\Radio\Group('radios', [1 => 'foo', 2 => 'bar']))
+        );
+        $this[] = new Formulaic\Label(
+            'Group of checkboxes',
+            (new Formulaic\Checkbox\Group(
+                'checkboxes',
+                [1 => 'foo', 2 => 'bar', 3 => 'baz']
+            ))
+        );
+    }
+}
+
