02a21b69d135fea7d4813b7d419b3f2183f83faf
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-22 10:26:38 +0100

    add method to check a radio/checkbox

--- src/Radio.php
+++ src/Radio.php
@@ -7,6 +7,15 @@ class Radio extends Element
     protected $attributes = ['type' => 'radio'];
     protected $value = 1;
 
+    public function check($value = null)
+    {
+        if ($value === false) {
+            unset($this->attributes['checked']);
+        } else {
+            $this->attributes['checked'] = $value;
+        }
+    }
+
     public function checked()
     {
         return array_key_exists('checked', $this->attributes);
