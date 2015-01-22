fbfd1743a710bd013a4313b46230bc31e9758291
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-22 13:59:47 +0100

    fix name/id generation

--- src/Element/Tostring.php
+++ src/Element/Tostring.php
@@ -12,15 +12,26 @@ trait Tostring
         if ($id = $this->id()) {
             $this->attributes['id'] = $id;
         }
-        if ($this->prefix && isset($this->attributes['name'])) {
+        if ($this->prefix
+            && isset($this->attributes['name'])
+            && !is_bool($this->attributes['name'])
+        ) {
             $parts = $this->prefix;
             $old = $this->attributes['name'];
             $parts[] = $old;
             $start = array_shift($parts);
             $this->attributes['name'] = $start;
             foreach ($parts as $part) {
-                $this->attributes['name'] .= "[$part]";
+                if (!is_bool($part)) {
+                    $this->attributes['name'] .= "[$part]";
+                }
             }
+        }
+        if (isset($this->attributes['name'])
+            && is_bool($this->attributes['name'])
+        ) {
+            $old = $this->attributes['name'];
+            unset($this->attributes['name']);
         }
         $out = '<input'.$this->attributes().'>';
         if (isset($old)) {
