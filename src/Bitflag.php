e0c82c9541045bdd1a6708c448a7f32eeb85b9ed
Marijn Ophorst <marijn@sensimedia.nl>
2015-07-21 19:41:37 +0200

    bitflag support

---
+++ src/Bitflag.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Formulaic;
+
+class Bitflag extends Checkbox\Group
+{
+    public function populate()
+    {
+        parent::populate();
+        foreach ((array)$this as $element) {
+            if ((is_array($this->source)
+                    && in_array($element->getValue(), $this->source)
+                )
+                || (!is_array($this->source)
+                    && $this->source & $element->getValue()
+                )
+            ) {
+                $element->check();
+            } else {
+                $element->check(false);
+            }
+        }
+    }
+
+    public function getValue()
+    {
+        $bit = 0;
+        foreach ((array)$this as $element) {
+            $bit |= $element->getValue();
+        }
+        return $bit;
+    }
+}
+
