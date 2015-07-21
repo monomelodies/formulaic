7199bf29fd339cc10be9b7c5dae1c82d5b4d8a16
Marijn Ophorst <marijn@sensimedia.nl>
2015-07-21 19:41:28 +0200

    add getArrayCopy method

--- src/Form.php
+++ src/Form.php
@@ -21,5 +21,23 @@ abstract class Form extends ArrayObject
             $this->attributes['name'] :
             null;
     }
+
+    public function getArrayCopy()
+    {
+        $copy = [];
+        foreach ((array)$this as $key => $value) {
+            if ($value instanceof Label) {
+                $value = $value->getElement();
+            }
+            if (is_object($value)
+                and method_exists($value, 'name')
+                and $name = $value->name()
+                and !($value instanceof Button)
+            ) {
+                $copy[$name] = $value->getValue();
+            }
+        }
+        return $copy;
+    }
 }
 
