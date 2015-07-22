d4ce47fe077dc42540179fd1da84c1d97d7dfc4c
Marijn Ophorst <marijn@sensimedia.nl>
2015-07-22 19:06:11 +0200

    setValue override

--- src/Bitflag.php
+++ src/Bitflag.php
@@ -22,6 +22,19 @@ class Bitflag extends Checkbox\Group
         }
     }
 
+    public function setValue($value)
+    {
+        for ($i = 1; $i < $value; $i *= 2) {
+            foreach ((array)$this as $element) {
+                if ($element->getValue() & $value == $value) {
+                    $element->check();
+                } else {
+                    $element->check(false);
+                }
+            }
+        }
+    }
+
     public function getValue()
     {
         $bit = 0;
