1c897e79a71e597b3b623c5039b0fbf8960292ae
Marijn Ophorst <marijn@sensimedia.nl>
2015-07-25 07:03:37 +0200

    comment better, and add method to get/set if value came from user input (needed during data binding)

--- src/Element.php
+++ src/Element.php
@@ -12,10 +12,17 @@ abstract class Element implements Labelable
     use Validate\Element;
 
     private $tests = [];
+    private $userInput = false;
     protected $prefix = [];
     protected $attributes = [];
     protected $value = null;
 
+    /**
+     * Constructor. The element's name is optional, but is usually something
+     * you'll want to provide.
+     *
+     * @param string $name The element's name.
+     */
     public function __construct($name = null)
     {
         if (isset($name)) {
@@ -24,11 +31,39 @@ abstract class Element implements Labelable
         $this->attributes['value'] =& $this->value;
     }
 
+    /**
+     * Sets the current value of this element.
+     *
+     * @param mixed $value The new value.
+     * @return Element $this
+     */
     public function setValue($value)
     {
         $this->value = $value;
     }
 
+    /**
+     * Gets or sets the origin of the current value (user input or bound).
+     * Normally, you won't need to call this directly since Formulaic handles
+     * data binding transparently.
+     *
+     * @param mixed $status null to get, true or false to set.
+     * @return boolean The current status (true for user input, false for
+     *                 undefined or bound from a model object).
+     */
+    public function valueSuppliedByUser($status = null)
+    {
+        if (isset($status)) {
+            $this->userInput = (bool)$status;
+        }
+        return $this->userInput;
+    }
+
+    /**
+     * Gets a reference to the current value.
+     *
+     * @return mixed The value.
+     */
     public function & getValue()
     {
         return $this->value;
@@ -36,27 +71,51 @@ abstract class Element implements Labelable
 
     /**
      * This is here to avoid the need to check instanceof Label.
+     *
+     * @return Element $this
      */
     public function getElement()
     {
         return $this;
     }
 
-    public function disabled($set = true)
+    /**
+     * Sets the elements disabled state. Note that this doesn't necessarily
+     * make sense for all elements, always (e.g. type="hidden").
+     *
+     * @param boolean $state True for disabled, false for enabled.
+     * @return Element $this
+     */
+    public function disabled($state = true)
     {
-        $this->attributes['disabled'] = $set;
+        $this->attributes['disabled'] = $state;
         return $this;
     }
 
+    /**
+     * Sets the placeholder text. Note that this doesn't necessarily make sense
+     * for all elements (e.g. type="radio").
+     *
+     * @param string $text The placeholder text.
+     * @return Element $this
+     */
     public function placeholder($text)
     {
         $this->attributes['placeholder'] = $text;
         return $this;
     }
 
+    /**
+     * Sets the tabindex. Note that the element can't know if the supplied value
+     * makes sense (e.g. is unique in the form), that's up to you.
+     *
+     * @param int $tabindex The tabindex to use.
+     * @return Element $this
+     */
     public function tabindex($tabindex)
     {
         $this->attributes['tabindex'] = (int)$tabindex;
+        return $this;
     }
 
     /** The field must equal the value supplied. */
