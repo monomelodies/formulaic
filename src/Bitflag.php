312ccaa86b9e5195dcf4e64fe24d4895af9e9efa
Marijn Ophorst <marijn@sensimedia.nl>
2015-07-25 09:19:37 +0200

    this will be passed as an array on submission, so if so call array_reduce on it

--- src/Bitflag.php
+++ src/Bitflag.php
@@ -8,6 +8,15 @@ class Bitflag extends Checkbox\Group
 
     public function setValue($value)
     {
+        if (is_array($value)) {
+            $value = array_reduce(
+                $value,
+                function ($carry, $item) {
+                    return $carry | $item;
+                },
+                0
+            );
+        }
         $this->value |= $value;
         foreach ((array)$this as $element) {
             if ($value & $element->getElement()->getValue()) {
