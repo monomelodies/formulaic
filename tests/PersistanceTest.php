ba71ecbb99ec42129a45e22ad819e4f78764d78f
Marijn Ophorst <marijn@sensimedia.nl>
2015-07-25 07:09:28 +0200

    swap order so data loading works

--- tests/PersistanceTest.php
+++ tests/PersistanceTest.php
@@ -4,11 +4,11 @@ class PersistanceTest extends PHPUnit_Framework_TestCase
 {
     public function testDataPersists()
     {
-        $form = new PersistantForm;
         $_POST['name'] = 'Linus';
+        $form = new PersistantForm;
         $user = new TestUserModel;
         $this->assertEquals('Marijn', $user->name);
-        $form->source($user);
+        $form->bind($user);
         $this->assertEquals('Linus', $user->name);
     }
 }
