e2b479ac5df4c3b3b594cf023f42c9033ce8cd6c
Marijn Ophorst <marijn@sensimedia.nl>
2015-07-25 06:39:21 +0200

    this needs its own set/getValue implementation

--- src/Checkbox/Group.php
+++ src/Checkbox/Group.php
@@ -8,6 +8,8 @@ class Group extends Radio\Group
 {
     use Group\Tostring;
 
+    private $value;
+
     public function populate()
     {
         parent::populate();
@@ -22,6 +24,31 @@ class Group extends Radio\Group
                 $element->check(false);
             }
         }
+    }
+
+    public function setValue($value)
+    {
+        if (is_scalar($value)) {
+            $value = [$value];
+        }
+        foreach ((array)$this as $element) {
+            if (in_array($element->getElement()->getValue(), $value)) {
+                $element->getElement()->check();
+            } else {
+                $element->getElement()->check(false);
+            }
+        }
+    }
+    
+    public function & getValue()
+    {
+        $this->value = [];
+        foreach ((array)$this as $element) {
+            if ($element->getElement()->checked()) {
+                $this->value[] = $element->getElement()->getValue();
+            }
+        }
+        return $this->value ? $this->value : null;
     }
 }
 
