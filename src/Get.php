d4aaf486b4aba25412bcc69325af0b60d0667c53
Marijn Ophorst <marijn@sensimedia.nl>
2016-02-08 17:55:20 +0100

    docblocks

--- src/Get.php
+++ src/Get.php
@@ -2,14 +2,33 @@
 
 namespace Formulaic;
 
+/**
+ * Implements a GET-form. The method and source default to that. You could
+ * override the method, but that would not make sense. Elements are
+ * automatically populated from `$_GET`.
+ */
 abstract class Get extends Form
 {
+    /**
+     * Returns the default string representation of this form.
+     *
+     * @return string The form as '<form>...</form>'.
+     * @see Formulaic\Form::__toString
+     */
     public function __toString()
     {
         $this->attributes['method'] = 'get';
         return parent::__toString();
     }
 
+    /**
+     * Adds an item to the form, checking to see if its $_GET-variant exists
+     * and if so, uses that as the value.
+     *
+     * @param integer|string|null $index The index to set the new item at.
+     * @param mixed $item An element or a label containing one.
+     * @return void
+     */
     public function offsetSet($index, $item)
     {
         $name = $item->getElement()->name();
@@ -17,7 +36,7 @@ abstract class Get extends Form
             $item->getElement()->setValue($_GET[$name]);
             $item->getElement()->valueSuppliedByUser(true);
         }
-        return parent::offsetSet($index, $item);
+        parent::offsetSet($index, $item);
     }
 }
 
