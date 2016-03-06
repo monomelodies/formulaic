0dacb0c534dcb9204656fd6d8008aa920f752fa0
Marijn Ophorst <marijn@sensimedia.nl>
2016-03-06 11:13:44 +0100

    except for the required test, empty fields should never be tested

--- src/Validate/Test.php
+++ src/Validate/Test.php
@@ -6,7 +6,12 @@ trait Test
 {
     public function addTest($name, callable $fn)
     {
-        $this->tests[$name] = $fn;
+        $this->tests[$name] = function ($value) use ($name, $fn) {
+            if (strlen(trim($value)) || $name == 'required') {
+                return $fn($value);
+            }
+            return true;
+        };
         return $this;
     }
 }
