1b20812acd12b93e88f43c157602ba72a4af4608
Marijn Ophorst <marijn@sensimedia.nl>
2016-06-10 18:52:00 +0200

    move to trait so selects can also use it

--- src/Element.php
+++ src/Element.php
@@ -6,6 +6,7 @@ abstract class Element implements Labelable
 {
     use Element\Tostring;
     use Element\Identify;
+    use Element\Wrap;
     use Attributes;
     use Validate\Test;
     use Validate\Required;
@@ -17,8 +18,6 @@ abstract class Element implements Labelable
     protected $prefix = [];
     protected $attributes = [];
     protected $value = null;
-    protected $htmlBefore = null;
-    protected $htmlAfter = null;
 
     /**
      * Constructor. The element's name is optional, but is usually something
@@ -143,21 +142,6 @@ abstract class Element implements Labelable
     public function tabindex($tabindex)
     {
         $this->attributes['tabindex'] = (int)$tabindex;
-        return $this;
-    }
-
-    /**
-     * Specify HTML to wrap this element in. Sometimes this is needed for
-     * fine-grained output control, e.g. when styling checkboxes.
-     *
-     * @param string $before HTML to prepend.
-     * @param string $after HTML to append.
-     * @return Element $this
-     */
-    public function wrap($before, $after)
-    {
-        $this->htmlBefore = $before;
-        $this->htmlAfter = $after;
         return $this;
     }
 
